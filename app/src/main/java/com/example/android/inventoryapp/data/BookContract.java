4a274fd4d97a1920273bf95bebdae5ba97fd1a3a
GuieMigi <florinchiriac2004@yahoo.com>
2018-05-23 19:37:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -0,0 +1,10 @@
+package com.example.android.inventoryapp.data;
+
+import android.provider.BaseColumns;
+
+public class BookContract {
+
+    public static final class BookEntry implements BaseColumns {
+
+    }
+}
---
+++ app/src/main/java/com/example/android/inventoryapp/data/BookDbHelper.java
@@ -0,0 +1,7 @@
+package com.example.android.inventoryapp.data;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BookDbHelper extends SQLiteOpenHelper {
+
+}
